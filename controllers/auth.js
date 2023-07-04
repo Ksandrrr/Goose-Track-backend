c4bdb24b2a1f37f9c4782d1e674784410f89b363
Ksandrrr <105207003+Ksandrrr@users.noreply.github.com>
2023-07-04 13:53:24 +0300

    update current

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,14 @@
-import bcrypt from "bcrypt"
-import  jwt from "jsonwebtoken"
-import gravatar from "gravatar"
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import gravatar from "gravatar";
 
-import {User} from "../models/auth.js"
+import { User } from "../models/auth.js";
 
-import ctrlWrapper from "../utils/ctrlWrapper.js"
+import ctrlWrapper from "../utils/ctrlWrapper.js";
 
-import  HttpError from "../helpers/HttpError.js"
+import HttpError from "../helpers/HttpError.js";
 
 const { SECRET_KEY } = process.env;
-
 
 async function register(req, res) {
   const { email, password } = req.body;
@@ -40,11 +39,15 @@ async function register(req, res) {
     user: {
       email: newUser.email,
       name: newUser.name,
-    }
+    },
   });
 }
 const updateUserInfo = async (req, res) => {
-    const updatedUser = await User.findByIdAndUpdate( req.user._id, { ...req.body }, { new: true });
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user._id,
+    { ...req.body },
+    { new: true }
+  );
   res.json({
     name: updatedUser.name,
     birthday: updatedUser.birthday,
@@ -53,61 +56,61 @@ const updateUserInfo = async (req, res) => {
     email: updatedUser.email,
     avatarURL: updatedUser.avatarURL,
   });
-}
-const login = async(req, res)=> {
-    const {email, password} = req.body;
-    const user = await User.findOne({email});
-    if(!user) {
-        throw HttpError(401, "Email or password invalid"); 
-    }
+};
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password invalid");
+  }
 
-    const passwordCompare = await bcrypt.compare(password, user.password);
-    if(!passwordCompare) {
-        throw HttpError(401, "Email or password invalid"); 
-    }
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password invalid");
+  }
 
-    const payload = {
-        id: user._id,
-    }
+  const payload = {
+    id: user._id,
+  };
 
-    const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "23h"});
-    await User.findByIdAndUpdate(user._id, { token })
-    
-    res.json({
-        token,
-        user: {
-            email: user.email,
-            name: user.name,
-            birthday: user.birthday,
-            phone: user.phone,
-            skype: user.skype,
-            avatarURL: user.avatarURL,
-        }
-})
-    
-}
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+  await User.findByIdAndUpdate(user._id, { token });
 
-const getCurrent = async(req, res)=> {
-   const {name, email,token,avatarURL,birthday,phone,skype} = req.user;
+  res.json({
+    token,
+    user: {
+      email: user.email,
+      name: user.name,
+      birthday: user.birthday,
+      phone: user.phone,
+      skype: user.skype,
+      avatarURL: user.avatarURL,
+    },
+  });
+};
 
-    res.json({
-        name, email,token,avatarURL,birthday,phone,skype
-    })
-}
+const getCurrent = async (req, res) => {
+  const { name, email, token, avatarURL, birthday, phone, skype } = req.user;
 
-const logout = async(req, res)=> {
-    const {_id} = req.user;
-    await User.findByIdAndUpdate(_id, {token: ""});
+  res.json({
+    token,
+    user: { name, email, token, avatarURL, birthday, phone, skype },
+  });
+};
 
-    res.status(204).json({
-        message: "Logout success"
-    })
-}
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
 
-export default  {
-    register: ctrlWrapper(register),
-    login: ctrlWrapper(login),
-    getCurrent: ctrlWrapper(getCurrent),
-    logout: ctrlWrapper(logout),
-    updateUserInfo: ctrlWrapper(updateUserInfo),
-}
+  res.status(204).json({
+    message: "Logout success",
+  });
+};
+
+export default {
+  register: ctrlWrapper(register),
+  login: ctrlWrapper(login),
+  getCurrent: ctrlWrapper(getCurrent),
+  logout: ctrlWrapper(logout),
+  updateUserInfo: ctrlWrapper(updateUserInfo),
+};
